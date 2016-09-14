87a400aa55f26df75aab2a4edf9f0f0415d75879
Xi Liu <Xi Liu>
2016-09-14 04:24:28 +0300

    New added

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var app = express();
+var neo4j = require("neo4j-driver").v1;
+
+
+//view Engine
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
+
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "250daowohao"));
+var session = driver.session();
+
+
+
+
+app.get("/",function(req,res){
+
+  session
+  .run("MATCH (m :Movie) RETURN m LIMIT 25")
+  .then(function(result){
+    var movieArr = [];
+
+    result.records.forEach(function(record) {
+      movieArr.push({
+        id:record._fields[0].identity.low,
+        title:record._fields[0].properties.title,
+        year: record._fields[0].properties.year
+      });
+      
+    });
+
+    res.render("index",{
+      movies:movieArr
+    });
+
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+
+  
+});
+
+app.post("/movie/add",function(req,res){
+  var title = req.body.title;
+  var year  = req.body.year;
+
+  session
+      .run(" CREATE (n:Movie {title:{titleParam}, year:{yearParam} }) RETURN n.title",{titleParam:title, yearParam:year} )
+      .then(function(result){
+        console.log(n.title);
+        res.redirect("/");
+        session.close();
+
+      })
+      .catch(function(err){
+        console.log(err);
+
+      })
+
+
+
+
+
+
+  res.redirect("/");
+
+});
+
+
+
+
+
+app.listen(3000);
+
+console.log("server started on port 3000");
+
+module.exports = app;
+
